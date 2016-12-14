bb5d1b73a450fa46f7d9cf16e4d871a73c1cd964
ToSchQLB <toni.schreiber.88@gmail.com>
2016-12-14 22:43:41 +0100

    fix Sender index

--- app/views/sender/index.php
+++ app/views/sender/index.php
@@ -48,36 +48,4 @@ HTML;
             ]) ?>
         </div>
     </div>
-
-    <div class="panel panel-default">
-    	  <div class="panel-heading">
-    			<h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
-    	  </div>
-    	  <div class="panel-body">
-                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-                <p>
-                    <?= Html::a(Yii::t('app', 'Create Sender'), ['create'], ['class' => 'btn btn-success']) ?>
-                </p>
-                <?php Pjax::begin(); ?>    <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'filterModel' => $searchModel,
-                        'columns' => [
-                            ['class' => 'yii\grid\SerialColumn'],
-
-                            'id',
-                            'name',
-                            'adress1',
-                            'adress2',
-                            'zip',
-                            // 'town',
-                            // 'state',
-                            // 'country',
-
-                            ['class' => 'yii\grid\ActionColumn'],
-                        ],
-                    ]); ?>
-                <?php Pjax::end(); ?>
-          </div>
-    </div>
 </div>
