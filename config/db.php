72ac1534e340ce28d1bccdfb37539c4ce40f4875
ToSchQLB <toni.schreiber.88@gmail.com>
2017-01-04 13:45:04 +0100

    update structure

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,23 @@
+<?php
+use kartik\datecontrol\Module;
+
+return [
+    'adminEmail' => 'admin@example.com',
+
+    // format settings for displaying each date attribute (ICU format example)
+    'dateControlDisplay' => [
+        Module::FORMAT_DATE => 'dd.MM.yyyy',
+        Module::FORMAT_TIME => 'hh:mm',
+        Module::FORMAT_DATETIME => 'dd.MM.yyyy hh:mm',
+    ],
+
+    // format settings for saving each date attribute (PHP format example)
+    'dateControlSave' => [
+        Module::FORMAT_DATE => 'php:Y-m-d', // saves as unix timestamp
+        Module::FORMAT_TIME => 'php:H:i',
+        Module::FORMAT_DATETIME => 'php:Y-m-d H:i',
+    ],
+
+    'dateControlDisplayTimezone' => 'Europe/Berlin',
+    'dateControlSaveTimezone' => 'Europe/Berlin',
+];
