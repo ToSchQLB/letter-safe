604c8786d42591434397a7acb51877874efd41cf
Toni Schreiber <toni.schreiber@jki.bund.de>
2018-06-05 10:27:22 +0200

    docker test

--- docker-install.sh
+++
@@ -1,4 +0,0 @@
-cd /var/www/html/letter-safe/
-echo "<?php [ 'class'=>'yii\db\Connection', 'dsn' => 'mysql:host=localhost;dbname=letter-safe', 'username' => 'letter-safe', 'password' => 'letter-safe', 'charset' => 'utf8',];" > ./config/db.php
-#wget https://getcomposer.org/composer.phar
-#php composer.phar install
--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM php:5.6-apache
-
-RUN apt-get update && apt-get -y install tesseract-ocr tesseract-ocr-deu imagemagick poppler-utils
-
-COPY ./ /var/www/html/letter-safe
-
-EXPOSE 80/tcp
