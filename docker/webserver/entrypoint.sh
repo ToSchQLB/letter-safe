f6e81b2048e99f2e5c20c163a1ff8b1509e83867
ToSchQLB <toni.schreiber.88@gmail.com>
2019-03-11 10:11:42 +0100

    Update entrypoint.sh

--- docker/webserver/entrypoint.sh
+++ docker/webserver/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /var/www/html/letter-safe/ \
-        &&  { \
-                echo '<?php'; \
-                echo 'return ['; \
-                echo '    "class" => "yii\db\Connection",'; \
-                echo '    "dsn" => "mysql:host=$LETTERSAFE_DB_SERVER;dbname=$LETTERSAFE_DB_DB",'; \
-                echo '    "username" => "$LETTERSAFE_DB_USER",'; \
-                echo '    "password" => "$LETTERSAFE_DB_PASSWORD",'; \
-                echo '    "charset" => "utf8",'; \
-                echo '];'; \
+        && { \
+            echo "<?php"; \
+            echo "return ["; \
+            echo "    'class' => 'yii\db\Connection',"; \
+            echo "    'dsn' => 'mysql:host=$LETTERSAFE_DB_SERVER;dbname=$LETTERSAFE_DB_DB',"; \
+            echo "    'username' => '$LETTERSAFE_DB_USER',"; \
+            echo "    'password' => '$LETTERSAFE_DB_PASSWORD',"; \
+            echo "    'charset' => 'utf8',"; \
+            echo "];"; \
         } > ./config/db.php \
         && cat ./config/db.php \
         && php yii migrate \
