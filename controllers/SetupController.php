183ab449274f988756ec346179bd9f91765599b8
ToSchQLB <toni.schreiber.88@gmail.com>
2019-03-12 16:43:58 +0100

    add DB-Setup

---
+++ controllers/SetupController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Toni
+ * Date: 12.03.2019
+ * Time: 16:39
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class SetupController extends Controller
+{
+    public function actionInitDb()
+    {
+        return '<pre>' .system('cd '. __DIR__ .'/../ && php yii migrate --interactive=0') . '</pre>';
+    }
+}
